6337e6d2321ebb8117bfa0f56447fee9ca31e9bd
Hung <hvngcs@gmail.com>
2023-01-07 11:25:27 +0000

    setup: update setup for Ubuntu

--- setup/install_common.sh
+++ setup/install_common.sh
@@ -4,35 +4,35 @@ case ${OSTYPE} in
     # For MacOS
     darwin*)
         echo "Hi MacOS!"
-	brew install neovim ctags tmux htop xclip the_silver_searcher wget
+    brew install neovim ctags tmux htop xclip the_silver_searcher wget
     ;;
 
     # For Linux
     linux*)
-	if [[ `lsb_release -is` == "Arch" ]]; then
-	    sudo pacman -Syyu
-	    sudo pacman -Syu neovim ctags tmux htop xclip the_silver_searcher wget --noconfirm
+    if [[ `lsb_release -is` == "Arch" ]]; then
+        sudo pacman -Syyu
+        sudo pacman -Syu neovim ctags tmux htop xclip the_silver_searcher wget --noconfirm
 
-	    # install yay
-	    pacman -Q yay
-	    if [[ $? -eq 0 ]]; then
-		echo "Yay is already installed"
-	    else
-		echo "Installing yay..."
-		git clone https://aur.archlinux.org/yay-git.git /tmp/yay-git
-		cd /tmp/yay-git
-		makepkg -si
-		yay -Syu
-		echo "DONE"
-	    fi
+        # install yay
+        pacman -Q yay
+        if [[ $? -eq 0 ]]; then
+        echo "Yay is already installed"
+        else
+        echo "Installing yay..."
+        git clone https://aur.archlinux.org/yay-git.git /tmp/yay-git
+        cd /tmp/yay-git
+        makepkg -si
+        yay -Syu
+        echo "DONE"
+        fi
 
-	elif [[ `lsb_release -is` == "Ubuntu" ]]; then
-	    sudo apt update
-	    sudo apt install neovim ctags tmux htop build-essential xclip silversearcher-ag curl -y
+    elif [[ `lsb_release -is` == "Ubuntu" ]]; then
+        sudo apt-get update -y
+        sudo apt-get install neovim universal-ctags tmux htop build-essential xclip silversearcher-ag curl -y
 
-	elif [[ `lsb_release -is` == "Fedora" ]]; then
-	    sudo dnf install neovim ctags tmux htop xclip the_silver_searcher -y
-	fi       
+    elif [[ `lsb_release -is` == "Fedora" ]]; then
+        sudo dnf install neovim ctags tmux htop xclip the_silver_searcher -y
+    fi       
     ;;
 esac
 
--- setup/install_yarn.sh
+++ setup/install_yarn.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # Installs Yarn (and nodejs if needed)
-sudo pacman -S yarn --noconfirm
+if [[ `lsb_release -is` == "Arch" ]]; then
+    sudo pacman -S yarn --noconfirm
+elif [[ `lsb_release -is` == "Ubuntu" ]]; then
+    sudo apt install yarn -y
+else
+    echo "Not supported, please install Hugo manually."
+fi
 
 echo "PATH=\"\$PATH:`yarn global bin`\"" >> ~/.bashrc
 echo "PATH=\"\$PATH:`yarn global bin`\"" >> ~/.zshrc
